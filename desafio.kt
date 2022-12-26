5fff55c62ee4db1a51e0e38b550a8b977090ce19
Erick Flanklin <erickflan10@yahoo.com>
2022-12-26 09:43:53 -0300

    Concluído o teste

--- desafio.kt
+++ desafio.kt
@@ -1,21 +1,35 @@
 // [Template no Kotlin Playground](https://pl.kotl.in/WcteahpyN)
 
-enum class Nivel { BASICO, INTERMEDIARIO, DIFICIL }
+enum class Nivel { BASICO, INTERMEDIARIO, AVANCADO }
 
 class Usuario
 
-data class ConteudoEducacional(var nome: String, val duracao: Int = 60)
+data class ConteudoEducacional(var nome: String, var duracao: Int = 60)
 
 data class Formacao(val nome: String, var conteudos: List<ConteudoEducacional>) {
 
     val inscritos = mutableListOf<Usuario>()
     
+    
     fun matricular(usuario: Usuario) {
-        TODO("Utilize o parâmetro $usuario para simular uma matrícula (usar a lista de $inscritos).")
+        inscritos.addAll(listOf(usuario))
+        //TODO("Utilize o parâmetro $usuario para simular uma matrícula (usar a lista de $inscritos).")
     }
 }
 
 fun main() {
-    TODO("Analise as classes modeladas para este domínio de aplicação e pense em formas de evoluí-las.")
-    TODO("Simule alguns cenários de teste. Para isso, crie alguns objetos usando as classes em questão.")
+    val conteudoEducacional = ConteudoEducacional("Matematica")
+    val formacao = Formacao("Tech", listOf(conteudoEducacional))
+    //TODO("Analise as classes modeladas para este domínio de aplicação e pense em formas de evoluí-las.")
+    
+    val nivel = Nivel.INTERMEDIARIO
+    val message = when(nivel){
+        Nivel.BASICO -> "BASICO"
+        Nivel.INTERMEDIARIO -> "INTERMEDIARIO"
+        Nivel.AVANCADO -> "AVANCADO"
+    }
+    println(message)
+    println(formacao)
+
+    //TODO("Simule alguns cenários de teste. Para isso, crie alguns objetos usando as classes em questão.")
 }
